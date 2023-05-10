cfd3e953e3f7ca88f8b918f70a8b0f35d12155ba
tanviAlam <tanvir9832@gmail.com>
2023-05-10 17:58:53 +0600

    add

--- frontend/src/pages/navbar/Navbar.jsx
+++ frontend/src/pages/navbar/Navbar.jsx
@@ -6,39 +6,47 @@ import "./navbar.css";
 import logo from "../../../public/Logo.png";
 import { useDispatch, useSelector } from "react-redux";
 import { userOnload } from "../../Reducers/userSlice";
+import FormatIndentDecreaseSharpIcon from "@mui/icons-material/FormatIndentDecreaseSharp";
+import FormatIndentIncreaseSharpIcon from "@mui/icons-material/FormatIndentIncreaseSharp";
+import { useState } from "react";
 
 const Navbar = () => {
   const { isAdmin, isLogin } = useSelector((state) => state.users);
+
+  const [isMenu, setIsMenu] = useState(false);
+
   const dispatch = useDispatch();
   const logOut = () => {
+    setIsMenu(!isMenu);
     localStorage.removeItem("codictionToken");
     dispatch(userOnload());
   };
   return (
-    <AppBar
-      className="navbar"
-      sx={{ backgroundColor: "#001a35" }}
-      position="sticky"
-    >
-      <div className="navbar_inside">
-        <div className="navbar_first">
-          <div>
-            <img className="Navbar_logo" src={logo} />
-          </div>
-          <div className="navbar_first_inside">
-            <Link className="link" to="/">
-              HOME
-            </Link>
-            <Link className="link" to="/course">
-              COURSE
-            </Link>
-            {isAdmin ? (
-              <Link className="link" to="/dashboard">
-                DASHBOARD
+    <>
+      <AppBar
+        className="navbar"
+        sx={{ backgroundColor: "#001a35" }}
+        position="sticky"
+      >
+        <div className="navbar_inside">
+          <div className="navbar_first">
+            <div>
+              <img className="Navbar_logo" src={logo} />
+            </div>
+            <div className="navbar_first_inside">
+              <Link className="link" to="/">
+                HOME
               </Link>
-            ) : null}
+              <Link className="link" to="/course">
+                COURSE
+              </Link>
+              {isAdmin ? (
+                <Link className="link" to="/dashboard">
+                  DASHBOARD
+                </Link>
+              ) : null}
+            </div>
           </div>
-        </div>
 
           <div className="navbar_second">
             {isLogin ? null : (
@@ -57,10 +65,54 @@ const Navbar = () => {
                 LOG OUT
               </button>
             ) : null}
-
           </div>
+
+          <div className="icon">
+            {isMenu ? (
+              <FormatIndentDecreaseSharpIcon
+                onClick={() => setIsMenu(!isMenu)}
+              />
+            ) : (
+              <FormatIndentIncreaseSharpIcon
+                onClick={() => setIsMenu(!isMenu)}
+              />
+            )}
+          </div>
+        </div>
+      </AppBar>
+      <div className={isMenu ? "menuUpdated" : "menu"}>
+        <div className="Alllinks">
+          <Link className="link" onClick={() => setIsMenu(!isMenu)} to="/">
+            HOME
+          </Link>
+          <Link className="link" onClick={() => setIsMenu(!isMenu)} to="/course">
+            COURSE
+          </Link>
+          {isAdmin ? (
+            <Link className="link" onClick={() => setIsMenu(!isMenu)} to="/dashboard">
+              DASHBOARD
+            </Link>
+          ) : null}
+          {isLogin ? null : (
+            <div className="newLogin">
+              <Link className="link" onClick={() => setIsMenu(!isMenu)} to="/login">
+                LOGIN
+              </Link>
+              <Link className="link" onClick={() => setIsMenu(!isMenu)} to="/register">
+                REGISTER
+              </Link>
+            </div>
+          )}
+
+          {isLogin ? (
+            <button className="logOut_button"  onClick={logOut}>
+              LOG OUT
+            </button>
+          ) : null}
+        </div>
+        <div></div>
       </div>
-    </AppBar>
+    </>
   );
 };
 
